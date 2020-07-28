4371c94c7546f7c943d534fa65f5b69a46d8fb5a
Stephen Worthington <stephen.john.worthington@gmail.com>
2020-07-28 20:22:41 +0100

    - Init project
    - Added what I expect will be all the dependencies needed
    - Duplicated the gitIgnore, will have fun tidying this up

---
+++ StackExchangeDemo/app/src/main/java/com/sworthington/stackexchangeapidemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sworthington.stackexchangeapidemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
